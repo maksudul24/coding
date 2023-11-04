6fc65c547e95fb5072a72db38e5a0a3ff8bde556
Maksudul Hasan <37172395+maksudul24@users.noreply.github.com>
2023-11-04 21:20:37 +0600

    Template

---
+++ sample.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define open_file freopen("input.txt","r",stdin)
+#define output_file freopen("output.txt","w",stdout)
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define dbug cout<<"Line: ",__LINE__<<"\n";
+#define INF 2147483647
+#define M_INF -2147483648
+#define SIZE 200010
+#define MOD 1000000009
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //use "\n" for next line
+
+    int t,n,x,y,m;
+
+    //open_file;
+    cin>>t;
+    while(t--){
+
+    }
+    return 0;
+}
