269063306bd604a9743be741ec1865541ba17d15
rudroXD <asifhassanrudro1@gmail.com>
2024-08-23 10:59:48 +0000

    2002Bw1

--- t.cpp
+++ t.cpp
@@ -21,7 +21,7 @@ int main()
     //use "\n" for next line
 
     int testCase;
-    int n,x,y,m,a,b,cnt,num,sum,temp;
+    int n,x,y,m,l,icl,k,ick;
     vector<int> alice(SIZE),bob(SIZE);
     bool sw;
     open_file;
@@ -31,10 +31,32 @@ int main()
         cin>>n;
         for(int i = 0; i < n; i++) cin>>alice[i];
         for(int i = 0; i < n; i++) cin>>bob[i];
+        if(alice[0] == bob[0]){
+            k = 0;
+            ick = 1;
+        }
+        else if(alice[0] == bob[n - 1]){
+            k = n - 1;
+            ick = -1;
+        }
+        else sw = false;
 
-        for(int i = 0, j = n - 1; i < n; i++, j--){
-            if(alice[i] != bob[i] && alice[i] != bob[j]) sw = false;
-            if(alice[j] != bob[j] && alice[j] != bob[i]) sw = false;
+        if(alice[n - 1] == bob[0]){
+            l = 0;
+            icl = 1;
+        }
+        else if(alice[n - 1] == bob[n - 1]){
+            l = n - 1;
+            icl = -1;
+        }
+        else sw = false;
+
+        for(int i = 0, j = n - 1; i < n && sw; i++, j--){
+            if(alice[i] != bob[k]) sw = false;
+            else k += ick;
+
+            if(alice[j] != bob[l]) sw = false;
+            else l += icl;
         }
         if(sw) cout<<"Bob\n";
         else cout<<"Alice\n";
