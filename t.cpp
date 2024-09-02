eaab93ab67998901d74b592b71528decbd13ee87
rudroXD <asifhassanrudro1@gmail.com>
2024-09-02 11:58:55 +0000

    2008c

--- t.cpp
+++ t.cpp
@@ -21,48 +21,22 @@ int main()
     //use "\n" for next line
 
     int testCase;
-    int len,x,row,n,cnt;
-    bool sw,swz;
-    string s;
+    int n,m,ans;
+    ll int temp;
+    double x;
     open_file;
     cin>>testCase;
     while(testCase--){
-        cin>>n;
-        cin.ignore();
-        cin>>s;
-        swz = false;
-        row = 2;
-        cnt = 0;
-        sw = true;
-        len = -1;
-
-        x = sqrt(n);
-        if(x * x != n) sw = false;
-
-        for(int i = 0; i < n && sw; i++){
-            if(swz){
-                x = (i + 1) % len;
-                if(x == 1 || x == 0){
-                   if(s[i] == '0') sw = false;
-                }
-                else if(s[i] == '1') sw = false;
-                if(x == 0) row++;
-                if(row == len) swz = false;
-            }
-            else if(s[i] == '0' && row != len){
-                len = i - 1;
-                swz = true;
-            }
-            else if(row == len){
-                if(s[i] == '0') sw = false;
-                else cnt++;
-            }
-        }
-        if(cnt != len || swz) sw = false;
-        if(n == 4 && s[0] == '1' && s[1] == '1' && s[2] == '1' & s[3] == '1') sw = true;
-        if(sw) YES;
-        else NO;
-
+        cin>>n>>m;
+        temp = m - n;
+        temp *= 8;
+        temp++;
+        x = sqrt(temp);
+        x = (x - 1) / 2.0;
+        ans = x;
+        ans++;
+        if(n == m) ans = 1;
+        cout<<ans<<"\n";
     }
     return 0;
 }
