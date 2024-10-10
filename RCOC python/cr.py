4822b544c64af3dd190fcd8ea1553b2cb41c9d7e
maksudul24 <maksudulrabat@gmail.com>
2024-10-10 20:06:28 +0600

    Red warning text added to cr.py

--- RCOC python/cr.py
+++ RCOC python/cr.py
@@ -2,6 +2,10 @@ import pyperclip as cp
 
 f = open('currentprogrammename.txt','w')
 x = cp.paste()
-f.write(x)
-print(x)
+if(len(x) > 0):
+    f.write(x)
+    print(x)
+else:
+    print('\033[31m !!!warning: ClipBoard Empty.\033[0m')
+
 f.close()
